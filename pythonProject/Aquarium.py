9fd01cad4a4e8f256529dbe242c5e61add2fbe88
Strahil731 <hristovstrahil5@gmail.com>
2024-10-31 09:45:33 +0200

    Create Aquarium.py

---
+++ pythonProject/Aquarium.py
@@ -0,0 +1,13 @@
+def aquarium():
+    long = int(input())
+    width = int(input())
+    height = int(input())
+    percent = float(input())
+
+    capacity = long * width * height
+    capacity_need_water = capacity / 1000
+    need_liter = capacity_need_water * (1 - percent / 1000)
+
+    print(need_liter)
+
+aquarium()
